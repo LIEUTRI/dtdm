a1b7a57a4b21c65f60ce44910fcb2411dba2db81
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-12 02:28:57 +0000

    Commit

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <h1>Chu de 6</h1>
 <Form name="myform" action="index.php" method="POST">
-<input name="my_tag" value="PILLS HERE"/>
+<input name="my_tag" value=""/>
 Ho ten: <input type="text" name="name" value=""/>
 Email: <input type="email" name="email" value=""/>
-<input type="submit" value="Submit" onClick="show()"/>
+<input type="submit" value="Submit"/>
 </Form>
 
 <?php
 echo $_POST['my_tag'];
 echo "<br></br>";
-print_r($_POST);
+#print_r($_POST);
+echo Chao ban $_POST['name'], email cua ban la $_POST['email'];
 ?>
