999f8e228d52910eaf0531517281a324db03086a
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-12 02:38:00 +0000

    Commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>Chu de 6</h1>
-<Form name="myform" action="index.php" method="POST">
+<Form name="myform" action="index.php" method="GET">
 <input type="hidden" name="my_tag" value=""/>
 Ho ten: <input type="text" name="name" value=""/>
 Email: <input type="email" name="email" value=""/>
@@ -9,6 +9,6 @@ Email: <input type="email" name="email" value=""/>
 <?php
 echo $_POST['my_tag'];
 echo "<br></br>";
-echo($_POST);
-#print_r(Chao ban $_POST['name'], email cua ban la $_POST['email']);
+
+echo "Chao ban " . $_GET['name'] ", email cua ban la " . $_GET['email']);
 ?>
