a5e5c672170a25ac2efe9bc86e86e2c39491fb4d
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-12 02:25:58 +0000

    Commit

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-Ho ten: <input type="text" name="name" value="">
-Email: <input type="email" name="email" value="">
-<input type="submit" >
+<h1>Chu de 6</h1>
+<Form name="myform" action="index.php" method="POST">
+<input name="my_tag" value="PILLS HERE"/>
+Ho ten: <input type="text" name="name" value=""/>
+Email: <input type="email" name="email" value=""/>
+<input type="submit" value="Submit" onClick="show()"/>
+</Form>
+
+<?php
+echo $_POST['my_tag'];
+echo "<br></br>";
+print_r($_POST);
+?>
