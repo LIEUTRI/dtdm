bc7e1c205c6306d0902b3164857337a3645f62bc
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-12 13:18:56 +0700

    commit

--- setup.php
+++ setup.php
@@ -1,10 +1,10 @@
 <?php
 function pg_connection_string_from_database_url() {
 	extract (parse_url($_ENV["DATABASE_URL"] ));
-	return "user=$user password=$pass host=$host dbname="_. substr($path, 1); # <- you may want to add sslmode=require there too
+	return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 $db = pg_connect(pg_connection_string_from_database_url() );
-if ('!$db){
+if (!$db){
 	echo "Error : Unable to open database\n";
 } else {
 	echo "Opened database successfully\n";
