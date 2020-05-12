e8fde655f93020a9bf6d491bbdd1326924891f59
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-12 02:32:16 +0000

    Commit

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ Email: <input type="email" name="email" value=""/>
 <?php
 echo $_POST['my_tag'];
 echo "<br></br>";
-print_r(Chao ban $_POST['name'], email cua ban la $_POST['email']);
+print_r($_POST);
+#print_r(Chao ban $_POST['name'], email cua ban la $_POST['email']);
 ?>
