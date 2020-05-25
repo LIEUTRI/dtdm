3d5c1495e31c66172553d1064c3b2bbba0891f9f
Minh Tri <trib1610701@student.ctu.edu.vn>
2020-05-25 22:46:28 +0700

    ...

--- list_account.php
+++ list_account.php
@@ -2,14 +2,14 @@
 <html>
 <header>
 <style>
-	error {color: #FFQ000;}
+	.error {color: #FF0000;}
 </style>
 </header>
 <body>
 <?php
 function pg_connection_string_from_database_url() {
 	extract (parse_url($_ENV["DATABASE_URL"] ));
-	return "user=$user password=$pass host=$host dbname="_. substr($path, 1); # <- you may want to add sslmode=require there too
+	return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 $db = pg_connect(pg_connection_string_from_database_url());
 if (!$db){
